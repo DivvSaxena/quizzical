6e5dd2baf71c979c52b1809300378dbe1f4a6e6a
divvsaxena <saxenadivv@gmail.com>
2024-06-19 18:07:31 +0530

    changed the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
 import React from 'react'
 import Questions from './components/Questions'
+import he from 'he'
 
 const App = () => {
 
   const [quiz , setQuiz] = React.useState([])
   const [value , setValue] = React.useState(false)
   const [toggle , setToggle] = React.useState(true)
+  
 
   const fetchdata = async () =>{
     console.log('Fetching data from API');
     try{
-        const response = await fetch('https://opentdb.com/api.php?amount=5')
+        const response = await fetch('https://opentdb.com/api.php?amount=5&type=multiple')
         if(!response.ok){
             throw new Error('Network response was not ok')
         }
         const data = await response.json()
+
+        //  console.log(data.results)
         
         const elements = data.results.map((item, index) => {
           return {
-            question: item.question,
-            key: index
+            question: he.decode(item.question),
+            key: index,
+            incorrect_answers: item.incorrect_answers,
+            correct_answer: item.correct_answer
           }
         })
 
         setQuiz(elements)
-    
     }catch (error){
         console.error(error.message)
     }
   }
   
+  // console.log(quiz)
 
   function handleButtonClick(){
     setValue(!(value))
@@ -55,14 +61,40 @@ const App = () => {
     </div>
   )
 
+  function arrayToShuffle(arrayToShuffle){
+    for(let i = arrayToShuffle.length - 1; i > 0 ; i--){
+      let randomPosition = Math.floor(Math.random() * ( i + 1 ))
+      let temp = arrayToShuffle[i]
+      //swap elements
+      arrayToShuffle[i] = arrayToShuffle[randomPosition]
+      arrayToShuffle[randomPosition] = temp
+    }
 
-    
+    return arrayToShuffle
+  }
+
    const mapper = quiz.map((item,index) => {
-    <Questions question={item.question} key={index}/>
+    let arr = item.incorrect_answers.concat(item.correct_answer)
+    let shuffledarr = arrayToShuffle(arr)
+    
+     return <Questions question={item.question} key={index} option={shuffledarr} correct_answer={item.correct_answer} />
    })
+
+   function handleSubmit(e){
+    e.preventDefault()
+    console.log('form submitted')
+   }
   
    const quizElement = (
-    {mapper}
+    <div className='quiz-page'>
+      <div className="irr-one"></div>
+      <div className='irr-two'></div>
+      <form className='forms' onSubmit={handleSubmit}>
+        {mapper}
+        <button className='btn-submit'>Submit</button>
+      </form>
+      
+    </div>
    )
 
 
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
 const Questions = (props) => {
+  // console.log('rendering question component')
+  // console.log(props.option)
+
+  const [state, setState] = React.useState(0)
+
+  function handleChange(e){
+    if(e.target.value == props.correct_answer){
+      console.log('matched')
+      setState((prevState) => prevState + 1)
+    }else{
+      console.log('not matched')
+    }
+  }
+
+  console.log(state)
+
+  const options_for_question = props.option.map((item) => {
+    return (<label>
+              <input type='radio' name='option' value={item} onChange={handleChange} />
+              {item}
+            </label>
+          )
+  })
 
   return (
-    <div>
-      <h1>{props.question}</h1>
-    </div>
+    <>
+      <div className='question'>
+        <h1>{props.question}</h1>
+        <form className='options-provided'>
+          {options_for_question}
+          <hr />
+        </form>
+      </div>
+    </>
+   
   )
 }
 
